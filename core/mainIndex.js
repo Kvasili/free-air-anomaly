a47739aa6cce43198c5762cc34042e2f0e826231
Kvasili <k.vasili913@gmail.com>
2021-02-22 22:26:31 +0200

    update

--- core/mainIndex.js
+++ core/mainIndex.js
@@ -1,3 +1,4 @@
+
 // main js for displaying map 
 // Author : Konsantinos vasili
 // Date: 10/2/21
@@ -16,8 +17,8 @@ var layersMap = [
             url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
             maxZoom: 19
 
-        }),
-        dem: false
+        })
+       
     })
 ]
 
@@ -27,20 +28,17 @@ var app = {
         layers: layersMap
      }),
 
-     extend:function(){
-        return [23.7, 38];
-     },
+    extend: [23.7, 38],
 
     render:function(mapDiv){ //render map
 
-        var extend = [23.7, 38];
         app._map.setTarget(mapDiv); 
         var viewMap = new ol.View({
-            center: ol.proj.fromLonLat(extend), //needs to be projected coords
+            center: ol.proj.fromLonLat(app.extend), //needs to be projected coords
             zoom : 6, 
             rotation : 0
         }); 
-        app._map.setView(viewMap);
+        app._map.setView(viewMap);  
     },
 
     uncheckBox:function(id1,id2){
@@ -48,6 +46,11 @@ var app = {
         document.getElementById(id1).checked = false; 
         document.getElementById(id2).checked = false; 
 
+    },
+
+    disableCalcButtons:function(id1,id2, bool){
+        document.getElementById(id1).disabled = bool;  
+        document.getElementById(id2).disabled = bool;  
     },
 
     calculateN:function(evt){
@@ -61,24 +64,43 @@ var app = {
         ui.paintGeo(geoidValue);
     },
 
+    removeMarker: function (){
+
+        var layersToRemove = [];
+    
+        this._map.getLayers().forEach(function (layer) {
+            if (layer.get('name') != undefined && layer.get('name') === 'vector') {
+                layersToRemove.push(layer);
+            }
+        });
+    
+        var len = layersToRemove.length;
+        for(var i = 0; i < len; i++) {
+            app._map.removeLayer(layersToRemove[i]);
+        }
+    },
+
     paintDivForScientificValue: function(){
 
         var checkBox1 = document.getElementById("checkbox1");
         var checkBox2 = document.getElementById("checkbox2");
         var checkBox3 = document.getElementById("checkbox3");
 
-        app._map.on('pointermove', app.calculateN);//because checkbox1 is initially checked
+        //var pointsForMarkers = []; 
+
+        this._map.on('pointermove', this.calculateN);//because checkbox1 is initially checked
 
         checkBox1.onchange = function(){
-
+            app.removeMarker();
             if(document.getElementById("checkbox1").checked){
                 app._map.on('pointermove', app.calculateN);
                 app.uncheckBox('checkbox2','checkbox3');
+                app.disableCalcButtons("buttonOfN1", "buttonOfN2", true);
         
             }else{
-                console.log('It works'); 
+                //console.log('It works'); 
                 ui.stopPaintingAll();
-                app._map.un('pointermove', app.calculateN);
+                app._map.un('pointermove', app.calculateN);//stop from calculating 
             }
         }
 
@@ -88,105 +110,169 @@ var app = {
                 app.uncheckBox('checkbox1','checkbox3');
                 ui.stopPaintingAll();
                 app._map.un('pointermove', app.calculateN);
+                app.disableCalcButtons("buttonOfN1", "buttonOfN2", false);
 
-                /// DO STUFF HERE ........ 
+                /// DO STUFF HERE 
+                var buttonA = document.getElementById("buttonOfN1"); 
+                var buttonB = document.getElementById("buttonOfN2");
 
+                buttonA.onclick = function(){
+                    app.removeMarker();
+                    var fi = document.getElementById('lat').value;
+                    var lamda = document.getElementById('lon').value;
+
+                    //pointsForMarkers.push([parseFloat(lamda), parseFloat(fi)]);
+                    putMarkers([[parseFloat(lamda), parseFloat(fi)]]); 
+                   
+                    var egsa = fl2EGSA87(fi, lamda); 
+                    ui.paintEgsa(egsa);
+                    
+                    var x = ol.proj.fromLonLat([parseFloat(lamda), parseFloat(fi)])[0];
+                    var y = ol.proj.fromLonLat([parseFloat(lamda), parseFloat(fi)])[1];
+                    var geoidValue = idw.idwPow2(x, y);
+                    ui.paintGeo(geoidValue);
+                }
+
+                buttonB.onclick = function(){
+                    app.removeMarker();
+                    var xegsa = document.getElementById('x').value;
+                    var yegsa = document.getElementById('y').value;
+                    //convert egsa coords to WGS84
+                    var fl84 = Egsa2fl84(xegsa, yegsa); 
+                    //console.log(fl84); 
+
+                    putMarkers([[parseFloat(fl84[1]), parseFloat(fl84[0])]]); 
+                    ui.paintWgs84([fl84[1], fl84[0]]);
+
+                    var x = ol.proj.fromLonLat([parseFloat(fl84[1]), parseFloat(fl84[0])])[0];
+                    var y = ol.proj.fromLonLat([parseFloat(fl84[1]), parseFloat(fl84[0])])[1];
+                    var geoidValue = idw.idwPow2(x, y);
+                    ui.paintGeo(geoidValue);                    
+                }
+
+
+
+
+            }else{
+                // DO STUFF HERE ........
+                app.disableCalcButtons("buttonOfN1", "buttonOfN2", true);
+                ui.stopPaintingAll();
+                app.removeMarker();
+                //putMarkers([[parseFloat(fl84[1]), parseFloat(fl84[0])]], false); 
+                //app._map.removeLayer(layerGroup);
+
+                
             }
         }
 
         checkBox3.onchange = function(){
-
+            app.removeMarker();
             if (checkBox3.checked) {
                 app.uncheckBox('checkbox1','checkbox2');
                 ui.stopPaintingAll();
                 app._map.un('pointermove', app.calculateN);
+                app.disableCalcButtons("buttonOfN1", "buttonOfN2", true);
 
                 /// DO STUFF HERE ........ 
             }
         }
-
-
 
     }
 
 }
 
-
 app.render("map"); 
-//app.validateCheckBox(); 
-app.paintDivForScientificValue(); 
-//app._map.on('pointermove', app.calculateN);
+app.paintDivForScientificValue();
 
-// app._map.on('pointermove', function (evt) {  //  , 'pointermove' 'click'
 
-//     //var map = evt.map;
-//     //get coordinates of center map view
-//     //center = ol.proj.toLonLat( map.getView().getCenter() );
-//     //console.log(center); 
+function putMarkers(places){
+
+    var vectorSource = new ol.source.Vector({});
+
+    for (var i = 0; i < places.length; i++) {
     
-//     //var fl = ol.proj.toLonLat( coordsXY );
-//     //console.log(ol.proj.toLonLat( coordsXY )); 
-
-//     //console.log(coordsXY);
+        var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.transform([places[i][0], places[i][1]], 'EPSG:4326', 'EPSG:3857')),
+        });
+       
+        var iconStyle = new ol.style.Style({
+            image: new ol.style.Circle({
+              radius: 6,
+              fill: new ol.style.Fill({
+                color: '#3399CC'
+              }),
+              stroke: new ol.style.Stroke({
+                color: '#fff',
+                width: 2
+              })
+            }),
+            crossOrigin: 'anonymous'
+          })
     
-//     var coordsXY = evt.coordinate;
-//     var egsa = fl2EGSA87(ol.proj.toLonLat( coordsXY )[1], ol.proj.toLonLat( coordsXY )[0]);
-//     var geoidValue = idw.idwPow2(coordsXY[0], coordsXY[1]);
-
-//     //console.log(egsa.toFixed(0));
-
-//     ui.paintWgs84(ol.proj.toLonLat( coordsXY )); 
-//     ui.paintEgsa(egsa);
-//     ui.paintGeo(geoidValue); 
+        iconFeature.setStyle(iconStyle);
+        vectorSource.addFeature(iconFeature);
+    }
     
-//     //var faa = idw.calculatePixelsDem(coordsXY[0], coordsXY[1]);
-//     //console.log(faa);
+    
+    var vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        // updateWhileAnimating: true,
+        // updateWhileInteracting: true
+    });
 
-//     //ui.paintEgsa(egsa);
-//     //console.log(geoidValue); 
-//     //ui.paintFaa(faa); 
-//     //ui.paintFaa2(faa); 
+    vectorLayer.set('name', 'vector');
+
+    app._map.addLayer(vectorLayer);
+    var viewMap = new ol.View({
+        center: ol.proj.fromLonLat([places[0][0], places[0][1]]), //needs to be projected coords
+        zoom : 10, 
+        rotation : 0
+    }); 
+    app._map.setView(viewMap); 
+}
+
+
+
+
+
+
+// var layerGroup = new ol.layer.Group({
+
+//     layers: [
+
+//         new ol.layer.Tile({
+    
+//             source : new ol.source.XYZ({
+                        
+//                 attributionsCollapsible: false,
+//                 url:'data/hellas-geoid-rgb-tiles/{z}/{x}/{y}.png',
+//                 maxZoom: 19
+        
+//             }),
+//             visible:true,
+//             //zIndex:2,
+//             dem: false
+            
+//         }),
+
+//         new ol.layer.Tile({
+    
+//             source : new ol.source.XYZ({
+                        
+//                 attributionsCollapsible: false,
+//                 url:'data/faa-rgb-tiles/{z}/{x}/{y}.png',
+//                 maxZoom: 19
+        
+//             }),
+//             visible:false,
+//             zIndex:2,
+//             dem: true,
+//             opacity: 1
+            
+//         })
+//     ],
 
 // })
-
-
-var layerGroup = new ol.layer.Group({
-
-    layers: [
-
-        new ol.layer.Tile({
-    
-            source : new ol.source.XYZ({
-                        
-                attributionsCollapsible: false,
-                url:'data/hellas-geoid-rgb-tiles/{z}/{x}/{y}.png',
-                maxZoom: 19
-        
-            }),
-            visible:true,
-            //zIndex:2,
-            dem: false
-            
-        }),
-
-        new ol.layer.Tile({
-    
-            source : new ol.source.XYZ({
-                        
-                attributionsCollapsible: false,
-                url:'data/faa-rgb-tiles/{z}/{x}/{y}.png',
-                maxZoom: 19
-        
-            }),
-            visible:false,
-            zIndex:2,
-            dem: true,
-            opacity: 1
-            
-        })
-    ],
-
-})
 
 
 //app._map.addLayer(layerGroup);
--- core/ui.js
+++ core/ui.js
@@ -4,31 +4,23 @@
 class UI{
     
     constructor(){
-        // this.egsa_coords = document.getElementById("egsa-coords"); 
-        // this.faa = document.getElementById('faa'); 
-        // this.faa2 = document.getElementById('faa2'); 
         this.egsaX = document.getElementById('x'); 
         this.egsaY = document.getElementById('y'); 
-
         this.wgs84F = document.getElementById('lat'); 
         this.wgs84L = document.getElementById('lon'); 
-
-
         this.geoValue = document.getElementById('N'); 
-
-
     }
 
 
     paintEgsa(egsa){
-        //this.egsa_coords.textContent = `X: ${egsa[0].toFixed(0)} ${'||'} Y:${egsa[1].toFixed(0)}`; 
+       
         this.egsaX.value = egsa[0].toFixed(0);
         this.egsaY.value = egsa[1].toFixed(0); 
     }
 
     paintWgs84(wgs){
-        this.wgs84F.value  =  wgs[1].toFixed(4); //`φ: ${wgs[1].toFixed(4)} ${'||'} λ:${wgs[0].toFixed(4)}`; 
-        this.wgs84L.value =  wgs[0].toFixed(4);
+        this.wgs84F.value  =  wgs[1].toFixed(3); 
+        this.wgs84L.value =  wgs[0].toFixed(3);
     }
 
     paintGeo(val){
